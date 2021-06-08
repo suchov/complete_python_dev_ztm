45004320d41d77ef45fe314e57af9027a832efe8
Artem Sychov <artem.suchov@gmail.com>
2021-06-08 20:35:16 +0300

    agumented assignment

--- test2.py
+++ test2.py
@@ -6,3 +6,7 @@ iq = 100 # this is a statement too - entire line
 # statment is the entire line under this one that is calculating and assigning
 user_age = iq / 5 # iq / 5 is espression
 
+# agumented assignment operator
+some_value = 5
+some_value += 2 # += -+ *=
+print(some_value)
