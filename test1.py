43196de69ebdb15e83cc47f8247270f42514dea2
Artem Sychov <artem.suchov@gmail.com>
2021-06-08 18:44:53 +0300

    variables and constants

--- test1.py
+++ test1.py
@@ -35,3 +35,16 @@ print(bin(5))
 
 # back to an int from a binary
 print(int('0b101', 2))
+
+# variables
+id = 190 # binding || assigning a 190 to a variable, id to value 190
+print(id)
+# variables rules for Python:
+# snake_case
+# starts with loewrcase or underscore
+# letters, numbers, underscores
+# case sensetive
+# dont' overwrite keywords
+
+# constants - jsut a variables rules but with uppercase
+PI = 3.14
