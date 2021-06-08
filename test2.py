e469086bc274e5d9a8e5921f65b4311c7b003c86
Artem Sychov <artem.suchov@gmail.com>
2021-06-08 20:31:21 +0300

    expression vs statement

---
+++ test2.py
@@ -0,0 +1,8 @@
+# expressions vs statements
+
+
+iq = 100 # this is a statement too - entire line
+
+# statment is the entire line under this one that is calculating and assigning
+user_age = iq / 5 # iq / 5 is espression
+
