2847341011a0021121c8c16ef20d0bbaf16fc636
Artem Sychov <artem.suchov@gmail.com>
2021-06-08 18:35:37 +0300

    finish on complex data type

--- test1.py
+++ test1.py
@@ -1,1 +1,32 @@
-print("test")
+# Fundamental Data Types - core to the language
+# int
+# float
+# complex
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+# Classes -> create our own custom types of data
+
+# Specialized Data Types -> packages and modules that we can use from libraries
+
+# None - nothing type
+print(2 + 4)
+print(type(2 + 4))
+print(type(2.2 + 4))
+
+# math functions https://docs.python.org/3/library/math.html
+print(round(3.1))
+print(abs(-20))
+
+# opeartor precedence(приоритет)
+print((20 - 3) + 2 ** 2)
+
+# ()
+# **
+# * /
+# + -
+
