4a5ddcb91e78fc12faffd19c00c183fcaa72e7dd
Artem Sychov <[artem.suchov@gmail.com]>
2023-06-14 22:20:10 +0200

    password exercise

---
+++ password.py
@@ -0,0 +1,9 @@
+# 2 inputs and then work with the string and print it
+
+passowrd = input('password please')
+secret = input('please give me secret')
+pass_length = len(passowrd)
+username = "Artem"
+
+
+print(f'{username}, you passowrd {passowrd} is {pass_length} and the secret is {secret}')
