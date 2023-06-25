11d5c5a847af407f9431921a8f0c4827412f075d
Artem Sychov <[artem.suchov@gmail.com]>
2023-06-25 21:55:41 +0200

    add good ternary example

---
+++ ternary.py
@@ -0,0 +1,8 @@
+# Ternary Operator
+
+# condition_if_true if condigion else contition_if_else
+
+is_frend = True
+can_message = "message allowed" if is_frend else "not allowed to message"
+
+print(can_message)
