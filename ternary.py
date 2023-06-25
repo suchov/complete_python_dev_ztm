9dcc62df27e55d54f4170455ed5dfd16c36b797c
Artem Sychov <[artem.suchov@gmail.com]>
2023-06-25 21:57:46 +0200

    change some logican mistake in comments

--- ternary.py
+++ ternary.py
@@ -1,6 +1,6 @@
 # Ternary Operator
 
-# condition_if_true if condigion else contition_if_else
+# condition_if_true if condigion else contition_if_false
 
 is_frend = True
 can_message = "message allowed" if is_frend else "not allowed to message"
